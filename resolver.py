9d67831fff91dac62b2138ce87ca3c1737f185fa
Yamnasm <jrts220@gmail.com>
2021-12-29 23:50:36 +0000

    Completed resolver
    - added:
    check for trilicates (101s and 202s)
    check for row duplicates and backtraced collisions

--- resolver.py
+++ resolver.py
@@ -21,18 +21,18 @@ boardX = [
 ]
 
 board12 = [
-    [0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 1],
-    [0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 1],
-    [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
-    [1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0],
-    [1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [0, 2, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0],
-    [0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 0, 2, 0, 2, 0, 0, 0, 1]
+    [1, 1, 2, 2, 1, 0, 0, 1, 2, 1, 2, 2],
+    [2, 2, 1, 2, 2, 1, 2, 1, 1, 2, 1, 1],
+    [1, 1, 2, 1, 2, 0, 0, 2, 1, 1, 2, 2],
+    [2, 2, 1, 2, 1, 2, 1, 1, 2, 2, 1, 1],
+    [2, 2, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
+    [1, 1, 2, 1, 2, 1, 2, 2, 1, 1, 2, 2],
+    [1, 1, 2, 2, 1, 2, 1, 2, 2, 1, 2, 1],
+    [2, 2, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2],
+    [1, 2, 2, 1, 1, 2, 1, 2, 2, 1, 1, 2],
+    [2, 1, 1, 2, 2, 1, 1, 2, 2, 1, 2, 1],
+    [1, 2, 2, 1, 1, 2, 2, 1, 1, 2, 1, 2],
+    [2, 1, 1, 2, 1, 2, 1, 2, 1, 2, 2, 1]
 ]
 
 
@@ -54,13 +54,13 @@ def checkpairs(table):
                     if 0 not in check and len(check) == 2: #dodgy fix logic. bad.
                         if check.count(check[0]) == len(check):
                             table[i][ii] = lazy(check[0])
-                            print(f"added {lazy(check[0])} at {i + 1}:{ii + 1} RIGHT")
+                            print(f"added {lazy(check[0])} at {i + 1}:{ii + 1} (right)")
                             return table
                     check = row[ii - 2:ii]
                     if 0 not in check and len(check) == 2:
                         if check.count(check[0]) == len(check):
                             table[i][ii] = lazy(check[0])
-                            print(f"added {lazy(check[0])} at {i + 1}:{ii + 1} LEFT")
+                            print(f"added {lazy(check[0])} at {i + 1}:{ii + 1} (left)")
                             return table
                 except IndexError:
                     continue
@@ -74,22 +74,73 @@ def checkpairs(table):
                     if 0 not in check and len(check) == 2: #dodgy fix logic. bad.
                         if check.count(check[0]) == len(check):
                             table[i][ii] = lazy(check[0])
-                            print(f"added {lazy(check[0])} at {i + 1}:{ii + 1} DOWN")
+                            print(f"added {lazy(check[0])} at {ii + 1}:{i + 1} (down)")
                             return list([list(a) for a in zip(*table)])
                     check = row[ii - 2:ii]
                     if 0 not in check and len(check) == 2:
                         if check.count(check[0]) == len(check):
                             table[i][ii] = lazy(check[0])
-                            print(f"added {lazy(check[0])} at {i + 1}:{ii + 1} UP")
+                            print(f"added {lazy(check[0])} at {ii + 1}:{i + 1} (up)")
                             return list([list(a) for a in zip(*table)])
                 except IndexError:
                     continue
     
 # check for triplicates (ie 2, 0, 2 and 1, 0, 1)
-
-# check for duplicate columns and rows
+def checktrip(table):
+    for i, row in enumerate(table):
+        for ii, cell in enumerate(row):
+            if cell == 0:
+                try:
+                    check = [row[ii + 1], row[ii - 1]]
+                    if 0 not in check:
+                        if check.count(check[0]) == len(check):
+                            table[i][ii] = lazy(check[0])
+                            print(f"added {lazy(check[0])} at {i + 1}:{ii + 1}")
+                            return table
+                except IndexError:
+                    continue
+    
+    table = list([list(a) for a in zip(*table)])
+    for i, row in enumerate(table):
+        for ii, cell in enumerate(row):
+            if cell == 0:
+                try:
+                    check = [row[ii + 1], row[ii - 1]]
+                    if 0 not in check: 
+                        if check.count(check[0]) == len(check):
+                            table[i][ii] = lazy(check[0])
+                            print(f"added {lazy(check[0])} at {ii + 1}:{i + 1}")
+                            return list([list(a) for a in zip(*table)])
+                except IndexError:
+                    continue
 
 # check for "impossibilities"
+def testforcollisions(table):
+    for i, row in enumerate(table):
+        if row.count(0) == 2:
+            for x in range(1, 2):
+                temp = row
+                for ii, cell in enumerate(temp):
+                    if cell == 0:
+                        temp[ii] = x
+                        break
+                for ii, cell in enumerate(temp):
+                    if cell == 0:
+                        temp[ii] = lazy(x)
+                for k, comp in enumerate(table):
+                    if i != k:
+                        if temp == comp:
+                            temp = row
+                            for ii, cell in enumerate(temp):
+                                if cell == 0:
+                                    temp[ii] = lazy(x)
+                                    break
+                            for ii, cell in enumerate(temp):
+                                if cell == 0:
+                                    temp[ii] = x
+                            table[i] = temp
+                            print(f"added pair on row {i}")
+                            return table
 
 # check row and column totals
 def checkmax(table):
@@ -133,11 +184,30 @@ def runtestloop(table):
         if temp:
             table = temp
             i += 1
+        temp = checktrip(table)
+        if temp:
+            table = temp
+            i += 1
+        temp = testforcollisions(table)
+        if temp:
+            table = temp
+            i += 1
         if infloop == i:
             print(f"Test complete. Halted after {i} steps")
             print("")
             [print(r) for r in table]
             break
+    if any(0 in rows for rows in table):
+        return
+    else:
+        print(f"Test completed after {i} steps")
+        print("")
+        [print(r) for r in table]
+
 
 if __name__ == "__main__":
-    runtestloop(board12)
+    runtestloop(boardX)
+    # [print(r) for r in board12]
+    # print("")
+    # board12 = testforcollisions(board12)
+    # [print(r) for r in board12]
